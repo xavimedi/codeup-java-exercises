36fa71de6bc455c72bcd7d5384e26706c9d09901
Xavier <xavier.g.medina3@gmail.com>
2023-06-15 14:01:02 -0500

    Bob and StringExercise complete

--- src/Bob.java
+++ src/Bob.java
@@ -2,6 +2,22 @@ import java.util.Scanner;
 
 public class Bob {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("What's up? My name is Bob.");
+		String input = scanner.nextLine();
+
+		if (input.isEmpty()){
+			System.out.println("Huh?");
+		}
+		else if(input.endsWith("?")){
+			System.out.println("Sure, dude.");
+		}
+		else if(input.endsWith("!")){
+			System.out.println("Hey, stop yelling at me.");
+		}
+		else {
+			System.out.println("I don't even know what you're saying, dude.");
+		}
 	}
 }
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 
 public class StringExercise {
 	public static void main(String[] args) {
+		String we = "We ";
+		String dNN = "don't need no ";
+		String edu = "education ";
+		String tC = "thought control ";
+		System.out.println(we + dNN + edu);
+		System.out.println(we + dNN + tC);
 
+		System.out.println("Check \"this\" out! \"s inside of \"s!");
+		System.out.println("In windows, the main drive is usually C:\\");
+		System.out.println("I can do backslashes \\, double back slashes \\\\, and the amazing triple backslash \\\\\\!");
 	}
 }
