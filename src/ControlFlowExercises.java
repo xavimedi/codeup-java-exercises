fd247550f6138ba53199a76211ac527ce002aa93
Xavier <xavier.g.medina3@gmail.com>
2023-06-15 09:45:05 -0500

    finished Control Statements and Loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,4 @@
+import java.util.GregorianCalendar;
 import java.util.Scanner;
 public class ControlFlowExercises {
 	public static void main(String[] args) {
@@ -54,25 +55,68 @@ public class ControlFlowExercises {
 //			 }
 //		}
 
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("What number would you like to go up to?");
+//		int userInput1 = Integer.parseInt(scanner.next());
+//
+//		System.out.println("Here's your table!");
+//		System.out.printf("%-7s| %-8s| %-6s%n", "number", "squared", "cubed");
+//		System.out.println("------ | ------- | -----");
+//
+//		for (int i = 1; i <= userInput1; i++){
+//			int square = (int)Math.pow(i, 2);
+//			int cube = (int)Math.pow(i, 3);
+//			System.out.printf("%-7d| %-8d| %-6d%n", i, square, cube);
+//
+//			}
+
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("What number would you like to go up to?");
-		int userInput1 = Integer.parseInt(scanner.next());
+		System.out.println("What's the number grade?");
+		int gradeInput = Integer.parseInt(scanner.next());
+		String gradeOutput = "";
 
-		System.out.println("Here's your table!");
-		System.out.println("number | squared | cubed");
-		System.out.println("------ | ------- | -----");
-
-		int i = 1;
-		for (;;){
-			if(i <= userInput1){
-				System.out.println(i + "      | "
-						+ i * i
-						+"       | "
-						+ (int)Math.pow(i, 3));
-				i++;
+		if (gradeInput >= 88 && gradeInput <= 100){
+			gradeOutput = "A";
+			if(gradeInput > 96){
+				gradeOutput = gradeOutput + "+";
 			}
+			if(gradeInput < 94){
+				gradeOutput = gradeOutput + "-";
+			}
+		}
+		else if (gradeInput >= 80  && gradeInput <= 87){
+			gradeOutput = "B";
+			if(gradeInput > 84){
+				gradeOutput = gradeOutput + "+";
+			}
+			if(gradeInput < 83){
+				gradeOutput = gradeOutput + "-";
+			}
+		}
+		else if (gradeInput >= 67 && gradeInput <= 79){
+			gradeOutput = "C";
+			if(gradeInput > 76){
+				gradeOutput = gradeOutput + "+";
+			}
+			if(gradeInput < 70){
+				gradeOutput = gradeOutput + "-";
+			}
+		}
+		else if (gradeInput >= 60 && gradeInput <= 66){
+			gradeOutput = "D";
+			if(gradeInput > 63){
+				gradeOutput = gradeOutput + "+";
+			}
+			if(gradeInput < 63){
+				gradeOutput = gradeOutput + "-";
+			}
+		}
+		else if (gradeInput <= 59){
+			gradeOutput = "F";
+		}
+		System.out.printf("That is a(n) %s%n", gradeOutput);
 
 		}
 
 	}
-}
+
